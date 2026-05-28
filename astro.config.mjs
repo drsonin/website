64cdcae10d82ba8449a71b24cc519e16d950a1ad
ai-wellmap <info@wellmap.co>
2026-05-28 21:54:29 +0300

    Enable sitemap generation

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 // @ts-check
 import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
     site: 'https://drsonin.com',
-    integrations: [mdx(), tailwind()],
+    integrations: [mdx(), sitemap(), tailwind()],
 });
