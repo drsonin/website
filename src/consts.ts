8a7071a6af3f71229301e2104d941ea8d13fa281
ai-wellmap <info@wellmap.co>
2026-05-28 20:02:40 +0300

    Initial Astro setup for drsonin.com

---
+++ src/consts.ts
@@ -0,0 +1,9 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Sonin Hambaravi | Dr. Dmitri Sonin';
+export const SITE_DESCRIPTION = 'Стоматологическая клиника в Таллине. Имплантация, протезирование, лечение зубов. Запись: +37253383036';
+export const SITE_URL = 'https://drsonin.com';
+export const CLINIC_PHONE = '+37253383036';
+export const CLINIC_EMAIL = 'info@drsonin.com';
+export const CLINIC_ADDRESS = 'Torupilli ots 2, 10113 Tallinn';
